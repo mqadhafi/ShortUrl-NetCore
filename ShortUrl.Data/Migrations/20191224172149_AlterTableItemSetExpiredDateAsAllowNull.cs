664ffdf1e822789386bbe5ce80690c2329d54430
Dhafi <mqadhafi@live.com>
2019-12-25 00:40:57 +0700

    Alter table "Item", set Column ExpiredDate to nullable

---
+++ ShortUrl.Data/Migrations/20191224172149_AlterTableItemSetExpiredDateAsAllowNull.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ShortUrl.Data.Migrations
+{
+    public partial class AlterTableItemSetExpiredDateAsAllowNull : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "ExpiredDate",
+                table: "Item",
+                type: "datetimeoffset",
+                maxLength: 7,
+                nullable: true,
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "datetimeoffset",
+                oldMaxLength: 7);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTimeOffset>(
+                name: "ExpiredDate",
+                table: "Item",
+                type: "datetimeoffset",
+                maxLength: 7,
+                nullable: false,
+                oldClrType: typeof(DateTimeOffset),
+                oldType: "datetimeoffset",
+                oldMaxLength: 7,
+                oldNullable: true);
+        }
+    }
+}
