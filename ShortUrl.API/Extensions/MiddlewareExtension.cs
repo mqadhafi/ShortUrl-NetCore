a41e78f813406557b5f5a43bc797db276e1b2fbc
Dhafi <mqadhafi@live.com>
2019-12-25 00:05:04 +0700

    Add Item.cs as entity class, this class will store all url that are shorten

---
+++ ShortUrl.API/Extensions/MiddlewareExtension.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ShortUrl.Data.Context;
+
+namespace ShortUrl.API.Extensions
+{
+    internal static class MiddlewareExtension
+    {
+        internal static void ConfigureDatabase(this IServiceCollection services, IConfiguration configuration)
+        {
+            // EF: DbContext
+            services.AddDbContext<ApplicationContext>(options => options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+        }
+    }
+}
---
+++ ShortUrl.Data/Entities/BaseEntity.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace ShortUrl.Data.Entities
+{
+    public abstract class BaseEntity
+    {
+        public Guid Id { get; set; }
+        public DateTimeOffset CreatedDate { get; set; }
+    }
+}
