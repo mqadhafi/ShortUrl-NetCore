a345883980a8966cb644912e9ddfd4759f3b58ef
Dhafi <mqadhafi@live.com>
2019-12-25 23:01:01 +0700

    Add automapper profile for Statistic Entity and Command Handler on WebAPI

---
+++ ShortUrl.API/Profiles/StatisticProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using ShortUrl.Commands.Models;
+using ShortUrl.Domain.Entities;
+
+namespace ShortUrl.API.Profiles
+{
+    public class StatisticProfile : Profile
+    {
+        public StatisticProfile()
+        {
+            CreateMap<CreateStatisticCommand, Statistic>();
+        }
+    }
+}
